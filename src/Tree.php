d31e809c3e7a3b05e4ab58f1f916ca5492f78361
adumitru68 <tempera@3698>
2019-02-28 21:03:43 +0200

    Tree usable class

---
+++ src/Tree.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Adi
+ * Date: 2/28/2019
+ * Time: 9:02 PM
+ */
+
+namespace Qpdb\Common;
+
+
+use Qpdb\Common\Prototypes\Abstracts\AbstractTree;
+
+class Tree extends AbstractTree
+{
+
+}
