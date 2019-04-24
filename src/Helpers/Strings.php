2c3105ff82e9036a52232e96b39f38ba158014c2
adumitru68 <tempera@3698>
2019-04-24 12:58:00 +0300

    Random string generator function

--- src/Helpers/Strings.php
+++ src/Helpers/Strings.php
@@ -171,4 +171,16 @@ class Strings
 		}
 	}
 
+	public function generateRandom( $length = 10 ) {
+		$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+		$characters = str_shuffle( '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ' );
+		$charactersLength = strlen( $characters );
+		$randomString = '';
+		for ( $i = 0; $i < $length; $i++ ) {
+			$randomString .= $characters[ rand( 0, $charactersLength - 1 ) ];
+		}
+
+		return $randomString;
+	}
+
 }
